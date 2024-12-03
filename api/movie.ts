a116a245df4cb57050c8a62986b656cde831f51c
jeons <etoile929@naver.com>
2024-12-03 21:33:54 +0900

    수정

--- api/movie.ts
+++ api/movie.ts
@@ -1,13 +1,13 @@
 import fetch from 'node-fetch'
 import { VercelRequest, VercelResponse } from '@vercel/node' 
 
-const { OMDB_APIKEY } = process.env
+const { APIKEY } = process.env
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
     const {title, page, id } = JSON.parse(request.body)
     const url = id 
-        ? `http://omdbapi.com?apikey=${OMDB_APIKEY}&i=${id}&plot=full`
-        : `http://omdbapi.com?apikey=${OMDB_APIKEY}&s=${title}&page=${page}`
+        ? `http://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`
+        : `http://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`
     const res = await fetch(url)
     const json = await res.json()
     response
